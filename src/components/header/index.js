8a5467652b305b86154b0c6af3b69956c41685e6
dananw <dananwijaya1996@gmail.com>
2021-03-29 14:30:10 +0700

    first commit

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+
+import './styles.scss';
+
+const Header = () => (
+  <div>
+    <Image className="banner" src="assets/drinkware_banner.jpg" />
+  </div>
+);
+
+export default Header;
